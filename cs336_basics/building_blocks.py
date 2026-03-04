43a2f29946fd41ef12f131f54ca5e9f8a40cb8d8
Zhuocheng Xu <zhuochex@amazon.com>
2026-03-04 07:37:09 -0800

    add transformer block

--- cs336_basics/building_blocks.py
+++ cs336_basics/building_blocks.py
@@ -53,9 +53,9 @@ class SWIGLU(torch.nn.Module):
         super().__init__()
         self.d_model = d_model
         self.d_ff = d_ff
-        self.w1 = Linear(d_ff, d_model)
-        self.w3 = Linear(d_ff, d_model)
-        self.w2 = Linear(d_model, d_ff)
+        self.w1 = Linear(d_model, d_ff)
+        self.w3 = Linear(d_model, d_ff)
+        self.w2 = Linear(d_ff, d_model)
 
     def forward(self, x: torch.Tensor):
         z = self.w1(x)
@@ -124,7 +124,7 @@ class MultiHeadAttention(torch.nn.Module):
         self.q_proj = Linear(d_model, d_model)
         self.k_proj = Linear(d_model, d_model)
         self.v_proj = Linear(d_model, d_model)
-        self.o_proj = Linear(d_model, d_model)
+        self.output_proj = Linear(d_model, d_model)
 
         if max_seq_len is not None and theta is not None:
             self.rope = RotaryPositionalEmbedding(theta, self.d_k, max_seq_len)
@@ -151,7 +151,23 @@ class MultiHeadAttention(torch.nn.Module):
         attn_out = scaled_dot_product_attention(Q, K, V, mask)
         # (..., num_heads, seq_len, d_v) -> (..., seq_len, num_heads * d_v)
         attn_out = attn_out.transpose(-3, -2).flatten(-2)
-        return self.o_proj(attn_out)
+        return self.output_proj(attn_out)
+
+
+class TransformerBlock(torch.nn.Module):
+
+    def __init__(self, d_model, num_heads, d_ff, max_seq_len=None, theta=None):
+        super().__init__()
+        self.ln1 = RMSNorm(d_model)
+        self.ln2 = RMSNorm(d_model)
+        self.attn = MultiHeadAttention(d_model, num_heads, max_seq_len, theta)
+        self.ffn = SWIGLU(d_model, d_ff)
+
+    def forward(self, x: torch.Tensor):
+        x = x + self.attn(self.ln1(x))
+        x = x + self.ffn(self.ln2(x))
+
+        return x
 
 
 
